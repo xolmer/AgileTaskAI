f8a5ef163fdd7b391d79ab4017e1a9ab871340dc
Kaveh Aidivandi <aidivandi.dev@gmail.com>
2023-08-17 19:59:04 +0200

    partial backend setup

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+'use client';
+
+import { useBoardStore } from '@/store/BoardStore';
+import React, { useEffect } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Board = () => {
-  return <div>Board</div>;
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  return (
+    <h1>Hello</h1>
+    // <DragDropContext onDragEnd={() => {}}>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div className="">{/* Rendering all the columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
+  );
 };
 
 export default Board;
