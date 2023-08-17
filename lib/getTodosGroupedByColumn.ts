20392580fe344c74678f92e9886c421b039ef06c
Kaveh Aidivandi <aidivandi.dev@gmail.com>
2023-08-17 20:20:37 +0200

    GetTodos function (Backend)

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -7,4 +7,27 @@ export const getTodosGroupedByColumn = async () => {
   );
 
   const todos = data.documents;
+
+  const columns = todos.reduce((acc, todo) => {
+    const column = todo.status;
+
+    if (!acc.get(column)) {
+      acc.set(column, {
+        id: column,
+        todos: [],
+      });
+    }
+
+    acc.get(column)!.todos.push({
+      $id: todo.$id,
+      $createdAt: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      ...(todo.image && { image: JSON.parse(todo.image) }),
+    });
+
+    return acc;
+  }, new Map<TypedColumn, Column>());
+
+  console.log('columns', columns);
 };
