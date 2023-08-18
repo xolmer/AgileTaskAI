aff2c4c63cb8c4cf6ac5f4a4132b4e2637876c3f
Kaveh Aidivandi <aidivandi.dev@gmail.com>
2023-08-18 19:35:20 +0200

    Drag and Drop cols and cards: DB and Client side

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,13 +6,108 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
 
 const Board = () => {
-  const [board, getBoard] = useBoardStore((state) => [state.board, state.getBoard]);
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+    state.setBoardState,
+    state.updateTodoInDB,
+  ]);
 
   useEffect(() => {
     getBoard();
   }, [getBoard]);
 
-  const handleOnDragEnd = (result: DropResult) => {};
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    //Check if user dropped the item outside of a droppable
+    if (!destination) return;
+
+    //Handle column drag and drop
+    if (type === 'column') {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+
+    //Handle card drag and drop
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    // const startCol: Column = {
+    //   id: startColIndex[0],
+    //   todos: startColIndex[1].todos,
+    // };
+    // const finishCol: Column = {
+    //   id: finishColIndex[0],
+    //   todos: finishColIndex[1].todos,
+    // };
+
+    //Provide a fallback in case the columns are undefined
+    const startCol = startColIndex && {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+    const finishCol = finishColIndex && {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      newTodos.splice(destination.index, 0, todoMoved);
+
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      const newColumns = new Map(board.columns);
+      newColumns.set(newCol.id, newCol);
+
+      setBoardState({
+        ...board,
+        columns: newColumns,
+      });
+    } else {
+      // Dragging from one Column to another
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(newCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      // Update the database
+
+      updateTodoInDB(todoMoved, finishCol.id);
+
+      setBoardState({
+        ...board,
+        columns: newColumns,
+      });
+    }
+  };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -4,6 +4,8 @@ import { create } from 'zustand';
 interface BoardState {
   board: Board;
   getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -13,5 +15,21 @@ export const useBoardStore = create<BoardState>((set) => ({
   getBoard: async () => {
     const board = await getTodosGroupedByColumn();
     set({ board });
+  },
+
+  setBoardState: (board) => set({ board }),
+
+  updateTodoInDB: async (todo, columnId) => {
+    const { databases } = await import('@/appwrite');
+
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
   },
 }));
