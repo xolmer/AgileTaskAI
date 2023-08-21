a4120e0fb377d346cf6e4dce80f5176e8dbebe20
Kaveh Aidivandi <aidivandi.dev@gmail.com>
2023-08-21 17:44:08 +0200

    AI

--- app/api/generate-summary/route.ts
+++ app/api/generate-summary/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
       },
       {
         role: 'user',
-        content: `Hi there, provide a summary of the following todos, count how many todos are in each category such as to do, in progress, and done, then tell the user which task is more important and limit the suggestion to 300 characters! Here's the data: ${JSON.stringify(
+        content: `Hi there, provide a summary of the following todos, count how many todos are in each category such as to do, in progress, and done, then tell the user which tasks are more important in (todo and inprogress category) and limit the suggestion to 400 characters! Here's the data: ${JSON.stringify(
           todos
         )}`,
       },
