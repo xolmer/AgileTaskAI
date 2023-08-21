83dc973e85e9f32001fce5ac2eb7d3ca112e73e0
Kaveh Aidivandi <aidivandi.dev@gmail.com>
2023-08-21 17:38:51 +0200

    s

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -6,6 +6,8 @@ export const getTodosGroupedByColumn = async () => {
     process.env.NEXT_PUBLIC_COLLECTION_ID!
   );
 
+  console.log('DATA====>>>>>>>>>', data);
+
   const todos = data.documents;
 
   const columns = todos.reduce((acc, todo) => {
